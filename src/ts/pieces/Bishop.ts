d0b257f929c567fe58922863757918b3b9528ad2
Sephiii <jfgimenez@hotmail.com>
2025-02-22 02:39:53 +0100

    feat: Add Bishop class implementation

---
+++ src/ts/pieces/Bishop.ts
@@ -0,0 +1,30 @@
+import { Piece } from './Piece';
+import { Color, Position, ValidMove } from '../types';
+import { ChessBoard } from '../Board';
+
+export class Bishop extends Piece {
+    constructor(color: Color, position: Position) {
+        super(color, position, 'bishop');
+    }
+
+    getValidMoves(board: ChessBoard): ValidMove[] {
+        const moves: ValidMove[] = [];
+        const directions: [number, number][] = [
+            [-1, -1], // Haut-gauche
+            [-1, 1],  // Haut-droite
+            [1, -1],  // Bas-gauche
+            [1, 1]    // Bas-droite
+        ];
+
+        for (const [rowDir, colDir] of directions) {
+            const directionMoves = this.getMovesInDirection(board, rowDir, colDir);
+            moves.push(...directionMoves);
+        }
+
+        return moves;
+    }
+
+    clone(): Bishop {
+        return new Bishop(this.color, {...this.position});
+    }
+}
